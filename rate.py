61ab1eea03a93f5b44d15b38b40495a4408ca112
Scripter899 <84051607+Scripter899@users.noreply.github.com>
2021-06-17 15:31:17 -0400

    Create rate.py

---
+++ rate.py
@@ -0,0 +1,9 @@
+
+vote = input('How much points will you give this app?')
+
+print('Thanks for voting!')
+
+#  I will make an actual backend and voting system soon but for now i'm short on time.
+#  Thanks
+#
+#  -Scripter899
